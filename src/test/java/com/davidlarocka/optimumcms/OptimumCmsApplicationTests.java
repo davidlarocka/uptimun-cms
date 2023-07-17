08cc54732ed0f3ed3bd7d640b33cd7edf03de322
David García <dgarciagonzalez@altavoz.net>
2023-07-17 14:56:06 -0400

    java src

---
+++ src/test/java/com/davidlarocka/optimumcms/OptimumCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidlarocka.optimumcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimumCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
